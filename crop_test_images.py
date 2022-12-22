0a6f9a224a9081d0a17ffa4e4047eb2f9bef2c30
Aniket Kharad <92918607+aniketkharad@users.noreply.github.com>
2022-12-22 16:43:33 +0530

    Add files via upload

---
+++ crop_test_images.py
@@ -0,0 +1,56 @@
+import os
+import csv
+import cv2
+import numpy as np 
+
+path_base = '../project/stanford-car-dataset-by-classes-folder'
+class_names = []
+with open(path_base + '/names.csv') as csvNmesFile:
+    carNames = csv.reader(csvNmesFile, delimiter = ';')
+    for row in carNames:
+        if(row[0] == 'Ram C/V Cargo Van Minivan 2012'):
+            row[0] = "Ram C-V Cargo Van Minivan 2012"
+        class_names.append(row[0])
+# This represents the lsit of all the classes of cars given. It totals to 196
+
+anno_test = {}
+with open(path_base + '/anno_test.csv') as testImage:
+    testingImages = csv.reader(testImage, delimiter = ',')
+    for row in testingImages:
+        anno_test[row[0]] = [row[1], row[2], row[3], row[4], row[5]]
+# The dictionary of the individual images of testing dataset
+
+imageLoc = path_base + '/car_data/car_data/test/'   #The list imageLoc with class names
+# imageLocList = os.listdir(imageLoc)
+# List of image classes
+os.chdir(imageLoc)
+
+
+for class_ in class_names :
+    images_in_class = os.listdir('./{}/'.format(class_))
+    os.chdir('./{}/'.format(class_))
+
+    i = 0
+    for eachImage in images_in_class :
+    #     # print(anno_test[eachImage][4], class_names[int(anno_test[eachImage][4])-1])
+        r0 = anno_test[eachImage][0]
+        r1 = anno_test[eachImage][1]
+        r2 = anno_test[eachImage][2]
+        r3 = anno_test[eachImage][3]
+        # print(eachImage,r0,r1,r2,r3,r4)
+
+        img = cv2.imread(eachImage)
+        imCrop = img[int(r1):int(r3), int(r0):int(r2)]
+        cv2.imwrite(eachImage, imCrop)
+
+        i+=1
+        fract = i / len(images_in_class)
+        print('     {:>2.2%}'.format(fract), end="\r")
+    
+
+    # print('Cropped images of\t\t{}'.format(classs_))
+    os.chdir('../')
+    print('Cropped images of {}'.format(class_))
+
+
+
